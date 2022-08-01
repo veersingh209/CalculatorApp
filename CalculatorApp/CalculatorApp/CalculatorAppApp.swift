7538fec51a90eb87468340f845dec257e600bf3a
Veer Singh <veersingh209@icloud.com>
2022-08-01 08:38:01 -0700

    inital project commit

---
+++ CalculatorApp/CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Veer Singh on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
